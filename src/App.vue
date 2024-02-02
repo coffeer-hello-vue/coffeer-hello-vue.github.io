1f6fd833b3ce404978374ef666f2277cc9b79989
CoffeerLatte <todty2312@gmail.com>
2024-02-02 11:26:06 +0900

    체조 업로드

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,37 @@
 <script setup>
 import {ref} from 'vue'
-import Child from './components/Child.vue'
-const pocketMoney = ref("퇴실 할 시간 입니다.");
+
+const show  = ref(true)
+const list = ref([1,2,3])
+const a= ref(true)
+function showList(){
+	this.show = !this.show
+
+}
+function addList(a){
+	a = list.value.length +1
+	list.value.push(a)
+}
+function delList(){
+	list.value.pop(list.value.length)
+}
+function  reverseList(){
+	const reversed = this.list.reverse()
+	return reversed
+	
+}
+
+		
 </script>
 
 <template>
+<button @click = "showList()">List 렌더링 on/off </button>
+ <button @click = "addList(a)">List 추가 </button>
+<button @click = "delList()">List 제거 </button>
+<button @click = "reverseList">List 뒤집기 </button>
 
-<Child :pocketMoney="pocketMoney" />
+<ul v-if = "show === true"><li v-for= "item of list">{{ item }} </li></ul>
+<p v-else-if= "list.length">List is not empty, but hidden.</p>
+<p v-else>List is empty </p>
 
 </template>
---
+++ src/money.vue
@@ -0,0 +1,11 @@
+<script setup>
+import {ref} from 'vue'
+import Child from './components/Child.vue'
+const pocketMoney = ref("퇴실 할 시간 입니다.");
+</script>
+
+<template>
+
+<Child :pocketMoney="pocketMoney" />
+
+</template>
